bcf9fd81dbdb04de27afb714bc56a3707d5996d5
javacantcode <kingharbubbles95@gmail.com>
2018-12-01 11:51:01 +1100

    nice maymay
    cool beans

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Mover : MonoBehaviour {
+
+    private MouseLook _mouseLook;
+
+    public Transform camera;
+
+	// Use this for initialization
+	void Start () {
+        _mouseLook = new MouseLook();
+        _mouseLook.clampVerticalRotation = false;
+        _mouseLook.Init(this.transform, camera);
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        _mouseLook.LookRotation(this.transform, camera);
+
+    }
+
+    private void FixedUpdate()
+    {
+        _mouseLook.UpdateCursorLock();
+    }
+}
--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/MouseLook.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/MouseLook.cs
@@ -36,8 +36,8 @@ namespace UnityStandardAssets.Characters.FirstPerson
             m_CharacterTargetRot *= Quaternion.Euler (0f, yRot, 0f);
             m_CameraTargetRot *= Quaternion.Euler (-xRot, 0f, 0f);
 
-            if(clampVerticalRotation)
-                m_CameraTargetRot = ClampRotationAroundXAxis (m_CameraTargetRot);
+            //if(clampVerticalRotation)
+              //  m_CameraTargetRot = ClampRotationAroundXAxis (m_CameraTargetRot);
 
             if(smooth)
             {
